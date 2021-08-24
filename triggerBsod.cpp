c10dccc55f90357697a790002c15afd989af9675
Sid <sidhys1@gmail.com>
2021-08-23 21:08:15 -0400

    Update triggerBsod.cpp

--- triggerBsod.cpp
+++ triggerBsod.cpp
@@ -8,6 +8,7 @@
 
 EXTERN_C NTSTATUS NTAPI RtAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
 
+/* Calling a Hard Error creates a snapshot of your memory, in %SystemRoot% with the name 'memory.dmp' (this file can take up tons of space, I'd advise to delete it after) */
 EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
     PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
 
